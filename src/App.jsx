818ab3284b20862b8ea631c788ac819f20402534
Pradeep-XT <delta@xtown.in>
2025-07-03 18:22:18 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
+// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 
 import PublicUrl from "./Public";
 import PublicEmergencyContact from "./EmergencyContacts";
 import EmergencyAlert from "./VerificationForm";
 
-const App = () => {
+export default function App() {
   return (
     <Routes>
-      <Route>
-        <Route path="/public/alert" element={<EmergencyAlert />} />
-        <Route
-          path="/public-url/:id"
-          element={
-            // <RequireVerification>
-            <PublicUrl />
-            // </RequireVerification>
-          }
-        />
-        <Route
-          path="/public-emergency/:id/:contactName"
-          element={
-            // <RequireVerification>
-            <PublicEmergencyContact />
-            // </RequireVerification>
-          }
-        />
-      </Route>
+      <Route path="/public/alert" element={<EmergencyAlert />} />
+      <Route path="/public-url/:id" element={<PublicUrl />} />
+      <Route
+        path="/public-emergency/:id/:contactName"
+        element={<PublicEmergencyContact />}
+      />
     </Routes>
   );
-};
-
-export default App;
+}
