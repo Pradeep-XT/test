63a4fae638059a5ecd90fcc969cf614c9a98e227
Pradeep-XT <delta@xtown.in>
2025-07-04 11:44:09 +0530

    Adding new

--- src/EmergencyContacts.jsx
+++ src/EmergencyContacts.jsx
@@ -1,7 +1,7 @@
 // src/pages/PublicEmergencyContact.jsx
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "../services/api";
+import api from "./api";
 
 export default function PublicEmergencyContact() {
   const { id } = useParams();
